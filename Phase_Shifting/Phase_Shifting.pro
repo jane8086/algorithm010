d617d70a0a74440c96c3519794dc2e85c6527e21
Oilgrim <robinziegler.heg@googlemail.com>
2018-11-18 21:09:56 +0100

    Added camera functions

--- Phase_Shifting/Phase_Shifting.pro
+++ Phase_Shifting/Phase_Shifting.pro
@@ -5,7 +5,15 @@ TARGET = Phase_Shifting
 CONFIG += console
 CONFIG -= app_bundle
 
+INCLUDEPATH += /usr/local/include/opencv
+LIBS += -L/usr/local/lib -lopencv_core -lopencv_imgcodecs -lopencv_highgui -lopencv_imgproc -lopencv_calib3d -lopencv_structured_light
+CONFIG += c++11
+
 TEMPLATE = app
 
-SOURCES += main.cpp
+SOURCES += main.cpp \
+    camera.cpp
+
+HEADERS += \
+    camera.h
 
--- Phase_Shifting/main.cpp
+++ Phase_Shifting/main.cpp
@@ -1,9 +1,28 @@
-#include <QCoreApplication>
+#include "flycapture/FlyCapture2.h"
+#include "camera.h"
 
-int main(int argc, char *argv[])
+int main(void)
 {
-    QCoreApplication a(argc, argv);
 
-    return a.exec();
+    //1. Connect and adjust camera
+    FlyCapture2::Camera camera;
+    FlyCapture2::CameraInfo caminfo;
+    camera_connect(camera, caminfo);
+    camera_adjust(camera);
+
+    //2. Create Phase and Gray code patterns
+
+
+
+    //3. Show and Capture Patterns
+
+
+
+    //4. Unwrap Phase Maps
+
+
+    //5. Create Point Correspondences
+
+
 }
 
