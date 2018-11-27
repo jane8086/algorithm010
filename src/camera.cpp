649d4fe33bf2bd8bbb251838ef7abe6070e3cb40
Oilgrim <robinziegler.heg@googlemail.com>
2018-11-27 11:41:17 +0100

    Added wrapping function with whole capture routine for dev without vsync

--- src/camera.cpp
+++ src/camera.cpp
@@ -1,9 +1,13 @@
 #include "flycapture/FlyCapture2.h"
 #include <iostream>
+#include "opencv2/opencv.hpp"
+
 
 
 using namespace FlyCapture2;
 using namespace std;
+using namespace cv;
+
 
 int camera_adjust(Camera &camera){
 
@@ -14,8 +18,8 @@ int camera_adjust(Camera &camera){
     Property prop_g;
     prop_g.type = GAIN;
     prop_g.autoManualMode = false;
-    Error error1 = camera.SetProperty(&prop);
-    Error error2 = camera.SetProperty(&prop_g);
+    FlyCapture2::Error error1 = camera.SetProperty(&prop);
+    FlyCapture2::Error error2 = camera.SetProperty(&prop_g);
     return 0;
 
 }
@@ -65,4 +69,67 @@ int camera_disconnect(Camera &camera){
         return -1;
     }
     return 0;
+}
+
+
+int camera_capture(Camera &camera, vector<cv::Mat> &patterns, vector<Mat> &patterns_captured){
+
+    if(!patterns.size()){
+        cout << "Pattern is empty!" << endl;
+        return -1;
+    }
+
+    //Create Frame for displaying Pictures
+    namedWindow("Pattern", WINDOW_NORMAL);
+    moveWindow("Pattern", 1024 + 316, -20);
+    setWindowProperty("Pattern", WND_PROP_FULLSCREEN, WINDOW_FULLSCREEN);
+
+    for(int pattern_i = 0; pattern_i < patterns.size(); pattern_i++){
+
+        imshow("Pattern", patterns[pattern_i]);
+        waitKey(100);
+        Image new_image;
+        FlyCapture2::Error error = camera.RetrieveBuffer(&new_image);
+        if (error != PGRERROR_OK) {
+
+          std::cout << "capture error" << std::endl;
+          continue;
+        }
+        Image grayscale;
+        new_image.Convert(FlyCapture2::PIXEL_FORMAT_MONO8, &grayscale);
+        unsigned int rowBytes =
+            (double)grayscale.GetReceivedDataSize() / (double)grayscale.GetRows();
+        Mat frame = Mat(grayscale.GetRows(), grayscale.GetCols(), CV_8U,
+                        grayscale.GetData(), rowBytes);
+
+        // checking if frame is not empty and showing camera informations
+        if (frame.data) {
+
+          patterns_captured.push_back(frame);
+          pattern_i++;
+
+          } else {
+        cout << "Could not save pattern" << pattern_i + 1 << endl;
+            return -1;
+          }
+    }
+
+    destroyWindow("Pattern");
+    waitKey(1);
+    return 1;
+  }
+
+
+int camera_routine(Camera & camera, vector<Mat> &patterns, vector<Mat> &patterns_captured){
+
+    // Information not used yet-----------
+    CameraInfo caminfo;
+    //------------------------------
+    camera_connect(camera, caminfo);
+    camera_adjust(camera);
+    camera_capture(camera, patterns, patterns_captured);
+    camera_disconnect(camera);
+
+    return 0;
+
 }
