f519ba2838348518fad7d52ca82f46c1820733f4
vhgnguyen <hg.nguyenv@gmail.com>
2018-12-10 20:46:01 +0100

    Add function to detect monitor screen

--- include/preprocessing.h
+++ include/preprocessing.h
@@ -1,5 +1,6 @@
 #ifndef PREPROCESSING_H
 #define PREPROCESSING_H
+
 #include <stdio.h>
 #include <math.h>
 #include "opencv2/opencv.hpp"
@@ -7,11 +8,26 @@
 
 using namespace std;
 using namespace  cv;
+
+
+
 /** @brief Converts vector of gray images into vector of binary images with constant threshold
  *
  */
 int convert_binary(std::vector<Mat> &graycode_images);
 
+
+/** @brief Reduce moire effect of captured phase shift images vector and save it to another vector
+ *  @tparam diameter adjust the strength of filter, suggest between 20 and 80
+ */
 int reduce_moire(vector<Mat> &phase_shift, vector<Mat> &dst_phase_shift,int diameter);
+
+
+/** @brief Detect contour of the screen
+ *  @tparam crop_amount crop the contour to avoid unwanted noise, DEFAULT crop_amount=1
+ */
+Mat detect_screen(int &period, int &amount_shifts,int crop_amount);
+
+
 
 #endif // PREPROCESSING_H
--- src/preprocessing.cpp
+++ src/preprocessing.cpp
@@ -1,28 +1,35 @@
 #include <stdio.h>
 #include <math.h>
 #include "opencv2/opencv.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+#include "tools.h"
+#include <iostream>
+
 using namespace  cv;
 using namespace std;
+
 
 int convert_binary(vector<Mat> &graycode_images){
 
     //Seting contanst threshold --> Need to be modular someday...
 
-    for(int image_i = 0; image_i < graycode_images.size(); image_i++){
-
+    for (int image_i = 0; image_i < graycode_images.size(); image_i++)
+    {
         threshold(graycode_images[image_i], graycode_images[image_i], 40, 1, THRESH_BINARY);
-
     }
 
 }
 
-int reduce_moire(vector<Mat> &phase_shift, vector<Mat> &dst_phase_shift,int diameter)
+
+int reduce_moire(vector<Mat> &phase_shift, vector<Mat> &dst_phase_shift,int diameter=20)
 {
     if (( diameter <= 0) || (diameter <=20))
     {
         cout << "Invalid value of diameter. Suggested: 20->80" << endl;
         return -1;
     }
+
     if (!dst_phase_shift.empty())
     {
         cout << "Destination vector must be empty" << endl;
@@ -39,7 +46,67 @@ int reduce_moire(vector<Mat> &phase_shift, vector<Mat> &dst_phase_shift,int diam
 }
 
 
-int detect_screen(vector<Mat> &ground_image)
+Mat detect_screen(int &period, int &amount_shifts,int crop_amount = 1)
 {
+    vector<Mat> ground_image;
+    vector<vector<Point>> contour;
 
+    load_image_ground(ground_image, amount_shifts, period); //first white then black
+
+    // detect screen by subtraction black and white images
+    Mat subtract = ground_image[0] - ground_image[1];
+    blur(subtract,subtract,Size(3,3)); // blur image for better result
+    threshold(subtract,subtract,20,255,THRESH_BINARY);
+
+    // find contour
+    findContours(subtract,contour,RETR_LIST,CHAIN_APPROX_SIMPLE);
+    if (contour.empty())
+    {
+        cout << "The screen is not fully shown! Please adjust camera distance" << std::endl;
+    }
+
+    // find the largest contour (screen)
+    int max_area, index = 0;
+    for (int i=0; i<contour.size();i++)
+    {
+        double area = contourArea(contour[i],false);
+        if (area > max_area)
+        {
+            max_area = area;
+            index = i;
+        }
+    }
+
+    // draw contour
+    Mat frame = Mat::zeros(subtract.size(),CV_8U);
+    drawContours(frame,contour,index,Scalar(255,255,255),CV_FILLED);
+
+    // crop contour
+    drawContours(frame,contour,index,Scalar(0,0,0),crop_amount);
+
+    // convert binary
+    threshold(frame,frame,40,1, THRESH_BINARY);
+
+    return frame;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
