399b3621d1457f5aa36929ff5eaca6d6b5791239
Oilgrim <robinziegler.heg@googlemail.com>
2018-12-14 19:25:27 +0100

    changed main so a routine with images is working

--- src/main.cpp
+++ src/main.cpp
@@ -16,33 +16,33 @@ int main(void)
     vector<Mat> patterns;
     Monitor monitor(SAMSUNG_CURVED);
     int periods = 4;
-    int amount_shifts = 4;
+    int amount_shifts = 3;
     int color_patterns = 0;
     int novel_method = 0;
 
     //Routine to create all phase_shifting patterns
     create_patterns_all(monitor.size_x, monitor.size_y, periods, patterns,amount_shifts);
 
-    //3. Show and Capture Patterns
-    FlyCapture2::Camera camera;
-    vector<Mat> patterns_captured;
-    camera_routine(camera, patterns, patterns_captured);
+//    //3. Show and Capture Patterns
+//    FlyCapture2::Camera camera;
+//    vector<Mat> patterns_captured;
+//    camera_routine(camera, patterns, patterns_captured);
 
 
-//    // detect screen
-//    Mat screen;
+    // detect screen
+    Mat screen;
 
-//    //Calculate absolute phasemaps
-//    vector<Mat> absolute_phasemaps;
-//    calculate_absolute_phasemaps(absolute_phasemaps, screen, amount_shifts, patterns.size(), color_patterns, novel_method);
+    //Calculate absolute phasemaps
+    vector<Mat> absolute_phasemaps;
+    calculate_absolute_phasemaps(absolute_phasemaps, screen, amount_shifts, patterns.size(), color_patterns, novel_method);
 
 
-//    //5. Create Point Correspondences
-//    vector<Point2f> image_points;
-//    vector<Point> points_world_pixel;
-//    vector<Point3f> points_world;
-//    calculate_realWorld_3d_coordinates(points_world, points_world_pixel, image_points, absolute_phasemaps[0], absolute_phasemaps[1],monitor, periods,screen);
-//    saveDatayml(image_points, points_world_pixel, points_world);
+    //5. Create Point Correspondences
+    vector<Point2f> image_points;
+    vector<Point> points_world_pixel;
+    vector<Point3f> points_world;
+    calculate_realWorld_3d_coordinates(points_world, points_world_pixel, image_points, absolute_phasemaps[0], absolute_phasemaps[1],monitor, periods,screen);
+    saveDatayml(image_points, points_world_pixel, points_world);
 
 
 
